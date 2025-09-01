81c58f951aefcf490063da73044c44a78bdcf955
GustavoFranco0 <franco.gustavo29@escola.pr.gov.br>
2025-09-01 15:18:48 -0300

    .

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ button.addEventListener("click", function(){
     const atual= document.querySelector(".ativo")
     const proximoPasso = "passo-" + this.getAtribute("data-proximo")
 
-    atual.classList.remove("ativo")
+    atual.classList.remove("ativo");
     document.getElementById(proximoPasso).classList.add("ativo");
 })
 })
